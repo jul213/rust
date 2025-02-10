1af9384f4cc99d0d42c2dd0bbd477c6dfb60d4f2
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-10 10:33:45 +0000

    recorrer el hashMap con un for

--- hashmap.rs
+++ hashmap.rs
@@ -4,3 +4,8 @@ let mut scores = HashMap.new();
 
 scores.insert(String::from("buenas tardes"), 17);
 scores.insert(String::from("buenas noches"), 20);
+
+
+for (key,values) in &scores{
+    println!("{key}-{scores}");
+}
