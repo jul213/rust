b7f23813d5c35cd2e11c00a48756b01670a853e7
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-14 12:13:32 +0000

    terminamos el test

--- test.rs
+++ test.rs
@@ -3,5 +3,19 @@ mod tests {
     use super::*;
 
     #[test]
-    
+
+    fn larger_smaller(){
+        let larger = Rectangle {
+            width: 8,
+            height: 9,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+
+        assert!(larger.can_hold(&smaller));
+    }
 }
